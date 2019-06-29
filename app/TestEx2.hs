6341a0cfaf318ecd9c3ed670d1e8f0a3dc40e55c
Javier Diego <javier.d.fernandez7@gmail.com>
2019-06-29 18:30:44 -0500

    Agregado README y otros archivos para medir el desempeño

---
+++ app/TestEx2.hs
@@ -0,0 +1,11 @@
+module Main where
+import ModelCheck
+import System.Environment
+import Text.Printf
+import System.Directory
+    
+main :: IO [Bool]
+main = do
+        args <- getArgs
+        d <- getCurrentDirectory
+        fileCheck $ d ++  "/test/testcodes/shift/T/shift" ++ head args ++ ".txt"
